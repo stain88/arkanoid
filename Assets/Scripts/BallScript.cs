84d823daa57d65ae52297171bcd8655112f86b16
stain88 <marcbaghdadi@gmail.com>
2016-02-05 12:23:25 +0000

    init commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	private bool ballIsActive;
+	private Vector3 ballPosition;
+	private Vector2 ballInitialForce;
+
+	//GameObject
+	public GameObject playerObject;
+
+	public AudioClip hitSound;
+
+	// Use this for initialization
+	void Start () {
+		//create the force
+		ballInitialForce = new Vector2(100.0f, 300.0f);
+
+		//set to inactive
+		ballIsActive = false;
+
+		//ball position
+		ballPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check for user input
+		if (Input.GetButtonDown("Jump")==true) {
+			//check if is the first play
+			if (!ballIsActive) {
+				//reset the force
+				GetComponent<Rigidbody2D>().isKinematic = false;
+				//add a force
+				GetComponent<Rigidbody2D>().AddForce(ballInitialForce);
+				//set ball active
+				ballIsActive = !ballIsActive;
+			}
+		}
+
+		if (!ballIsActive && playerObject != null) {
+			//get and use the player position
+			ballPosition.x = playerObject.transform.position.x;
+
+			//apply player X position to the ball
+			transform.position = ballPosition;
+		}
+
+		//check if ball falls
+		if (ballIsActive && transform.position.y <-6) {
+			ballIsActive = !ballIsActive;
+			ballPosition.x = playerObject.transform.position.x;
+			ballPosition.y = -4.0f;
+			transform.position = ballPosition;
+			
+			GetComponent<Rigidbody2D>().isKinematic = true;
+
+			GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
+			//send message
+			player.SendMessage("TakeLife");
+		}
+	}
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (ballIsActive) {
+			GetComponent<AudioSource>().PlayOneShot(hitSound);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float playerVelocity;
+	private Vector3 playerPosition;
+	public float boundary;
+	private int playerLives;
+	private int playerPoints;
+
+	public AudioClip pointSound;
+	public AudioClip lifeSound;
+
+	// Use this for initialization
+	void Start () {
+		// get the initial position of the game object
+		playerPosition = gameObject.transform.position;
+
+		playerLives = 3;
+		playerPoints = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//horizontal movement
+		playerPosition.x += Input.GetAxis("Horizontal")*playerVelocity;
+
+		//leave the game
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+
+		//boundaries
+		if (playerPosition.x < -boundary) {
+			playerPosition.x = -boundary;
+		}
+		if (playerPosition.x > boundary) {
+			playerPosition.x = boundary;
+		}
+		
+		//update the game object transform
+		transform.position = playerPosition;
+
+		//check game state
+		WinLose();
+	}
+
+	void addPoints(int points) {
+		playerPoints += points;
+		GetComponent<AudioSource>().PlayOneShot(pointSound);
+	}
+
+	void TakeLife() {
+		playerLives--;
+		GetComponent<AudioSource>().PlayOneShot(lifeSound);
+	}
+
+	void OnGUI() {
+		GUI.Label (new Rect(5.0f,3.0f,200.0f,200.0f), "Lives: " + playerLives + " Score: " + playerPoints); 
+	}
+
+	void WinLose() {
+		//restart the game
+		if (playerLives==0) {
+			Application.LoadLevel("Level1");
+		}
+
+		//blocks destroyed
+		if ((GameObject.FindGameObjectsWithTag("Block")).Length==0) {
+			//check the current level
+			if (Application.loadedLevelName=="Level1") {
+				Application.LoadLevel("Level2");
+			} else {
+				Application.Quit();
+			}
+		}
+	}
+}
